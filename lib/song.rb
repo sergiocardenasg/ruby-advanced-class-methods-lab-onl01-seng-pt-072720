3e10d16e11e2a02b916ff5b6a9caa91afc50b66d
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-14 02:25:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,9 +35,9 @@ class Song
 
   def self.find_or_create_by_name(name)
     if self.all.include?(name)
-      self.find_by_name
+      find_by_name
     else
-      self.create_by_name
+      create_by_name
     end
   end
 
